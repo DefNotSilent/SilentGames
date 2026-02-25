e04baa42fd90ae8e697e09f802c42624d0a91807
DefNotSilent <refined.silent@my.com>
2026-02-24 22:47:41 -0800

    Create panic.js

---
+++ json/panic.js
@@ -0,0 +1,10 @@
+[
+  { "name": "Schoology", "url": "https://lms.lausd.net" },
+  { "name": "Google Classroom", "url": "https://classroom.google.com" },
+  { "name": "Clever", "url": "https://clever.com" },
+  { "name": "Canvas", "url": "https://canvas.instructure.com" },
+  { "name": "Google", "url": "https://google.com" },
+  { "name": "Khan Academy", "url": "https://www.khanacademy.org" },
+  { "name": "Infinite Campus", "url": "https://www.infinitecampus.com" },
+  { "name": "PowerSchool", "url": "https://www.powerschool.com" }
+]
